a1f287b730624ea17a30a2d1e02daec771ac7c1f
rebeccaleighfoster <rebecca.leigh.foster@gmail.com>
2019-10-02 16:57:39 -0500

    fixed order

--- src/app.js
+++ src/app.js
@@ -7,28 +7,27 @@ const { NODE_ENV } = require('./config')
 
 const app = express()
 
-app.get('/', (rep,res)=>{
-    res.send('hello, Becca!')
-})
-
-const morganOption = (NODE.ENV ==='production')
+const morganOption = (NODE_ENV === 'production')
   ? 'tiny'
   : 'common';
 
-app.use(function errorHandler(error, req, res, next) {
-       let response
-       if (NODE_ENV === 'production') {
-         response = { error: { message: 'server error' } }
+app.use(morgan(morganOption))
+app.use(cors())
+app.use(helmet())
+
+app.get('/', (req, res) => {
+    res.send('Hello, world!')
+})
+
+  app.use(function errorHandler(error, req, res, next) {
+      let response
+      if (NODE_ENV === 'production') {
+        response = { error: { message: 'server error' } }
       } else {
         console.error(error)
-         response = { message: error.message, error }
-       }
-       res.status(500).json(response)
-     })
-
-
-app.use(morgan(morganOption))
-app.use(helmet())
-app.use(cors())
+        response = { message: error.message, error }
+      }
+      res.status(500).json(response)
+    })
 
 module.exports = app
